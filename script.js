1fb5871274f9a9e6f7ee91be4392fee554b59763
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-05-03 17:20:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+async function getData (){
+    const javob = await fetch("https://ipinfo.io/json");
+    const data = await javob.json();
+    console.log(data);
+    
+}
+
+
+
+const box = document.getElementById("box");
+const btn = document.getElementById("btn");
+
+
+function chizish(malumot){
+    box.innerHTML=`
+    <h1>Foydalanuvchi joylashuvini aniqlash</h1>
+    <button id="btn">Joylashuvni ko'rsat</button>
+    <p>Kenglik: ${malumot.loc.split(",")[0]}</p>
+    <p>Uzunlik: ${malumot.loc.split(",")[1]}</p>
+    <p>Mamlakat: ${malumot.country}</p>
+    <p>Shahar: ${malumot.city}</p>
+    <p>Ip manzil: ${malumot.ip}</p>
+    <p>Internet Provayder: ${malumot.org}</p>
+    <div id="map">
+    <iframe
+    width="100%"
+    height="300"
+    frameborder="0" style="border:0;border-radius:20px"
+    src="https://www.google.com/maps?q=${
+        malumot.loc.split(",")[0]
+    },${malumot.loc.split(",")[1]}&output=embed"allowfullscreen>
+    </iframe>
+    </div>
+    
+    `
+
+}
+
+btn.addEventListener("click"()=>{
+    getData();
+})
